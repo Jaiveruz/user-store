f5cfd1a62d56460cde6264ae2f20057b5af05ec0
jaiver <juzcategui@ks2.com>
2025-08-08 17:29:30 -0400

    terminar

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,15 +1,37 @@
 import { Response, Request } from "express";
+import { CustomError, RegisterUserDto } from "../../domain";
+import { AuthService } from "../services/auth.service";
+import { UserModel } from "../../data";
 
 
 
 export class AuthController {
 
 
-    constructor() {}
+    constructor(
+        public readonly authService: AuthService
+    ) {}
 
     registerUser = async (req: Request, res: Response) => {
+
+        const [error, registerDto] = RegisterUserDto.create(req.body);
+        if (error) return res.status(400).json({ error });
+
+        this.authService.registerUser(registerDto!)
+            .then( data => res.json(data) )
+
+
+        try {
+            const user = new UserModel(registerDto);
+            await user.save();
+
+            return user;
+        } catch (error) {
+           throw CustomError.internalServer(`${error}`);
+        }
+
         // Implement registration logic here
-        res.status(201).json({ message: 'registerUser' });
+        res.json(registerDto);
     }
 
     loginUser = async (req: Request, res: Response) => {
